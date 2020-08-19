2dd2557c0dd5408e1f33c58071ff2749ced0376f
IndrajeetKane <kaneindrajeet1202@gmail.com>
2020-08-19 07:47:39 -0400

    new commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import faker from "faker";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
+      <ApprovalCard>
+        <div>
+          <h4>Warning !</h4>
+          Are you sure ?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 5:00 AM"
+          content="Nice blog post"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 6:00 AM"
+          content="Nice Subject"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 5:00 AM"
+          content="Good post"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
